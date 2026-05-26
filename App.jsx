d7391103ccc36ea028dba716f059728e4376f39c
Abraham Tiruneh <abraham.tiruneh7@gmail.com>
2026-05-26 21:54:25 +0300

    Initialize main React application setup

---
+++ App.jsx
@@ -0,0 +1,23 @@
+﻿import { Navigate, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import StudentRoutes from "./pages/StudentRoutes";
+import ParentRoutes from "./pages/ParentRoutes";
+import SchoolRoutes from "./pages/SchoolRoutes";
+import MainNav from "./components/shared/MainNav";
+
+export default function App() {
+  return (
+    <>
+      <MainNav />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/student/*" element={<StudentRoutes />} />
+        <Route path="/parent/*" element={<ParentRoutes />} />
+        <Route path="/school/*" element={<SchoolRoutes />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
+  );
+}
